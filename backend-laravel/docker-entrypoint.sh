624e1eb58b81fab2e9a61827a6c208ca5a41dd04
Mohan <vkmohan112@gmail.com>
2025-12-25 01:34:00 +0530

    changes in dockerfile due to server 500 error

--- backend-laravel/docker-entrypoint.sh
+++ backend-laravel/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
+php artisan migrate --force || true
 php artisan config:clear || true
 php artisan cache:clear || true
-php artisan migrate --force || true
 
 exec php artisan serve --host 0.0.0.0 --port "${PORT:-8000}"
